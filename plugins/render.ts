d60e97741dc91cc1ae5d2c241d603c42ce7a519b
Pooya Parsa <pyapar@gmail.com>
2021-07-14 13:33:21 +0200

    feat: bundle default messages

--- plugins/render.ts
+++ plugins/render.ts
@@ -1,10 +1,11 @@
 import { promises as fsp } from 'fs'
-import { resolve, join, dirname } from 'upath'
+import { resolve, join, dirname, basename } from 'upath'
 import type { Plugin } from 'vite'
 import Critters from 'critters'
 import glob from 'globby'
 import template from 'lodash.template'
 import htmlMinifier from 'html-minifier'
+import genericMessages from '../templates/messages.json'
 
 const r = (...path: string[]) => resolve(join(__dirname, '..', ...path))
 
@@ -17,12 +18,15 @@ export const RenderPlugin = () => {
       const critters = new Critters({ path: distDir })
       const htmlFiles = await glob(r('dist/templates/**/*.html'))
 
-      for (const file of htmlFiles) {
+      for (const fileName of htmlFiles) {
+        // Infer template name
+        const templateName = basename(dirname(fileName))
+
         // eslint-disable-next-line no-console
-        console.log('Processing', file)
+        console.log('Processing', templateName)
 
         // Read source template
-        let html = await fsp.readFile(file, 'utf-8')
+        let html = await fsp.readFile(fileName, 'utf-8')
 
         // Apply criters to inline styles
         html = await critters.process(html)
@@ -31,18 +35,20 @@ export const RenderPlugin = () => {
 
         // Remove the null scripts that are created by `virtual:windi.css`
         // TODO: Remove empty assets from dist and keep the rest with CDN import transform
-        if (file.includes('templates')) {
-          html = html.replace(/<script [^>]*>[\s\S]*?<\/script>/g, '')
-        }
+        html = html.replace(/<script [^>]*>[\s\S]*?<\/script>/g, '')
 
         // Minify HTML
         html = htmlMinifier.minify(html, { collapseWhitespace: true })
 
+        // Load messages
+        const messages = JSON.parse(await fsp.readFile(r(`templates/${templateName}/messages.json`), 'utf-8'))
+
         // Serialize into a js function
-        const jsTemplate = template(html, {
-          interpolate: /{{([\s\S]+?)}}/g,
-          variable: 'messages'
-        }).toString()
+        const jsCode = `
+const _messages = ${JSON.stringify({ ...genericMessages, ...messages })};
+const _render = ${template(html, { interpolate: /{{([\s\S]+?)}}/g }).toString()};
+const _template = (messages) => _render({ messages: { ..._messages, ...messages } });
+        `.trim()
 
         // Generate types
         const types = [
@@ -52,14 +58,14 @@ export const RenderPlugin = () => {
 
         // Write new template
         await Promise.all([
-          fsp.writeFile(file.replace('/index.html', '.cjs'), `module.exports.template = ${jsTemplate}`),
-          fsp.writeFile(file.replace('/index.html', '.mjs'), `export const template = ${jsTemplate}`),
-          fsp.writeFile(file.replace('/index.html', '.d.ts'), types)
+          fsp.writeFile(fileName.replace('/index.html', '.cjs'), `${jsCode}\nmodule.exports.template = _template`),
+          fsp.writeFile(fileName.replace('/index.html', '.mjs'), `${jsCode}\nexport const template = _template`),
+          fsp.writeFile(fileName.replace('/index.html', '.d.ts'), types)
         ])
 
         // Remove original html file
-        await fsp.rm(file)
-        await fsp.rmdir(dirname(file))
+        await fsp.unlink(fileName)
+        await fsp.rmdir(dirname(fileName))
       }
     }
   }
