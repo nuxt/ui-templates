8ad106462c8e08b1d30bd29a2b5b684bcb9d39fc
Pooya Parsa <pyapar@gmail.com>
2021-07-14 14:58:52 +0200

    improvements
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- plugins/render.ts
+++ plugins/render.ts
@@ -5,6 +5,7 @@ import Critters from 'critters'
 import glob from 'globby'
 import template from 'lodash.template'
 import htmlMinifier from 'html-minifier'
+import { camelCase } from 'scule'
 import genericMessages from '../templates/messages.json'
 
 const r = (...path: string[]) => resolve(join(__dirname, '..', ...path))
@@ -17,6 +18,8 @@ export const RenderPlugin = () => {
       const distDir = r('dist')
       const critters = new Critters({ path: distDir })
       const htmlFiles = await glob(r('dist/templates/**/*.html'))
+
+      const templateExports = []
 
       for (const fileName of htmlFiles) {
         // Infer template name
@@ -33,9 +36,26 @@ export const RenderPlugin = () => {
         // We no longer need references to external CSS
         html = html.replace(/<link[^>]*>/g, '')
 
-        // Remove the null scripts that are created by `virtual:windi.css`
-        // TODO: Remove empty assets from dist and keep the rest with CDN import transform
-        html = html.replace(/<script [^>]*>[\s\S]*?<\/script>/g, '')
+        // Inline SVGs
+        const svgSources = Array.from(html.matchAll(/src="([^"]+)"|url([^)]+)/g))
+          .map(m => m[1])
+          .filter(src => src.match(/\.svg$/))
+
+        for (const src of svgSources) {
+          const svg = await fsp.readFile(r('dist', src), 'utf-8')
+          const base64Source = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`
+          html = html.replaceAll(src, base64Source)
+        }
+
+        // Inline our scripts
+        const scriptSources = Array.from(html.matchAll(/<script[^>]*src="(.*)"[^>]*>[\s\S]*?<\/script>/g))
+          .filter(([_block, src]) => src.match(/^\/.*\.js$/))
+
+        for (const [scriptBlock, src] of scriptSources) {
+          let contents = await fsp.readFile(r('dist', src), 'utf-8')
+          contents = contents.replaceAll('/* empty css               */', '').trim()
+          html = html.replace(scriptBlock, contents.length ? `<script>${contents}</script>` : '')
+        }
 
         // Minify HTML
         html = htmlMinifier.minify(html, { collapseWhitespace: true })
@@ -44,29 +64,41 @@ export const RenderPlugin = () => {
         const messages = JSON.parse(await fsp.readFile(r(`templates/${templateName}/messages.json`), 'utf-8'))
 
         // Serialize into a js function
-        const jsCode = `
-const _messages = ${JSON.stringify({ ...genericMessages, ...messages })};
-const _render = ${template(html, { interpolate: /{{([\s\S]+?)}}/g }).toString()};
-const _template = (messages) => _render({ messages: { ..._messages, ...messages } });
-        `.trim()
+        const jsCode = [
+          `const _messages = ${JSON.stringify({ ...genericMessages, ...messages })}`,
+          `const _render = ${template(html, { interpolate: /{{([\s\S]+?)}}/g }).toString()}`,
+          'const _template = (messages) => _render({ messages: { ..._messages, ...messages } })'
+        ].join('\n').trim()
 
         // Generate types
         const types = [
-          'declare const template: (data: Record<string, any>) => string',
+          `const defaultMessages = ${JSON.stringify(messages)}`,
+          'export type DefaultMessages = typeof defaultMessages',
+          'declare const template: (data: Partial<DefaultMessages>) => string',
           'export { template }'
         ].join('\n')
 
+        // Register exports
+        templateExports.push({
+          exportName: camelCase(templateName),
+          templateName,
+          types
+        })
+
         // Write new template
-        await Promise.all([
-          fsp.writeFile(fileName.replace('/index.html', '.cjs'), `${jsCode}\nmodule.exports.template = _template`),
-          fsp.writeFile(fileName.replace('/index.html', '.mjs'), `${jsCode}\nexport const template = _template`),
-          fsp.writeFile(fileName.replace('/index.html', '.d.ts'), types)
-        ])
+        await fsp.writeFile(fileName.replace('/index.html', '.mjs'), `${jsCode}\nexport const template = _template`)
+        await fsp.writeFile(fileName.replace('/index.html', '.d.ts'), `${types}`)
 
         // Remove original html file
         await fsp.unlink(fileName)
         await fsp.rmdir(dirname(fileName))
       }
+
+      // Write an index file with named exports for each template
+      const contents = templateExports.map(exp => `export { template as ${exp.exportName} } from './templates/${exp.templateName}.mjs'`).join('\n')
+      await fsp.writeFile(r('dist/index.mjs'), contents, 'utf8')
+
+      await fsp.writeFile(r('dist/index.d.ts'), contents.replaceAll(/\.mjs/g, ''), 'utf8')
     }
   }
 }
