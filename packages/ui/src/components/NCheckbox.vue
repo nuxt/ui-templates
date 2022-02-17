e967cadd50e4c1654d5445598e277acc94cd0e3a
soroush ebadi <soroush.e89@gmail.com>
2022-02-17 11:09:01 +0330

    fix checkbox problem on UI by removing extra transform

--- packages/ui/src/components/NCheckbox.vue
+++ packages/ui/src/components/NCheckbox.vue
@@ -10,26 +10,29 @@
       class="absolute op0 peer"
       :disabled="disabled"
       @keypress.enter="checked = !checked"
+    />
+    <span
+      class="n-transition n-checkbox-box n-checked:n-checkbox-box-checked peer-active:n-active-base peer-focus-visible:n-focus-base"
     >
-    <span class="n-transition n-checkbox-box n-checked:n-checkbox-box-checked peer-active:n-active-base peer-focus-visible:n-focus-base">
-      <NIcon class="n-transition n-checkbox-icon transform scale-0 op0 n-checked:op100 n-checked:scale-100" />
+      <NIcon class="n-transition n-checkbox-icon op0 n-checked:op100" />
     </span>
     <span><slot /></span>
   </label>
 </template>
 
 <script setup lang="ts">
-import { useVModel } from '@vueuse/core'
+import { useVModel } from "@vueuse/core";
 
 const props = withDefaults(
   defineProps<{
-    modelValue?: boolean
-    disabled?: boolean }>(),
+    modelValue?: boolean;
+    disabled?: boolean;
+  }>(),
   {
     modelValue: false,
-    disabled: false
+    disabled: false,
   }
-)
-const emit = defineEmits<{(...args: any): void}>()
-const checked = useVModel(props, 'modelValue', emit, { passive: true })
+);
+const emit = defineEmits<{ (...args: any): void }>();
+const checked = useVModel(props, "modelValue", emit, { passive: true });
 </script>
