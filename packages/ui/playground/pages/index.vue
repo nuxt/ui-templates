f3ac1b105c6c91eb851076cafd5aac61f52e7336
soroush ebadi <soroush.e89@gmail.com>
2022-02-21 15:47:36 +0330

    change some lint config

--- packages/ui/playground/pages/index.vue
+++ packages/ui/playground/pages/index.vue
@@ -8,7 +8,9 @@
       </NTip>
 
       <div class="flex justify-between items-center">
-        <div class="text-4xl">Nuxt UI</div>
+        <div class="text-4xl">
+          Nuxt UI
+        </div>
       </div>
 
       <div class="flex gap-1 mb-5">
@@ -35,22 +37,32 @@
       </div>
 
       <NCard class="p4">
-        <div class="n-header-upper">Buttons</div>
+        <div class="n-header-upper">
+          Buttons
+        </div>
         <div class="flex flex-wrap gap-3 items-center">
-          <NButton n="yellow6 dark:yellow5 xs"> XS Yellow </NButton>
+          <NButton n="yellow6 dark:yellow5 xs">
+            XS Yellow
+          </NButton>
           <NButton n="orange6 dark:orange5 sm dashed">
             S Orange Dashed
           </NButton>
           <NButton n="red6 dark:red5 solid" icon="carbon:at">
             Red Solid
           </NButton>
-          <NButton disabled> Disabled </NButton>
-          <NButton n="purple6 dark:purple5 xl"> XL Purple </NButton>
+          <NButton disabled>
+            Disabled
+          </NButton>
+          <NButton n="purple6 dark:purple5 xl">
+            XL Purple
+          </NButton>
         </div>
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">Checkboxes</div>
+        <div class="n-header-upper">
+          Checkboxes
+        </div>
         <div class="flex gap-3 items-center">
           <NCheckbox n="red6 dark:sky5 sm" :model-value="true">
             Small
@@ -65,15 +77,23 @@
       </NCard>
 
       <NCard class="p4" n="green">
-        <div class="n-header-upper">Links</div>
+        <div class="n-header-upper">
+          Links
+        </div>
         <form class="flex gap-3 items-center">
-          <NLink to="/"> NuxtLink </NLink>
-          <NLink href="https://v3.nuxtjs.org/"> Nuxtjs.org </NLink>
+          <NLink to="/">
+            NuxtLink
+          </NLink>
+          <NLink href="https://v3.nuxtjs.org/">
+            Nuxtjs.org
+          </NLink>
         </form>
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">Radios</div>
+        <div class="n-header-upper">
+          Radios
+        </div>
         <form class="flex gap-3 items-center">
           <NRadio v-model="radio" n="red6 dark:red5" name="name" value="a">
             Apple
@@ -98,11 +118,19 @@
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">Switches</div>
+        <div class="n-header-upper">
+          Switches
+        </div>
         <div class="flex gap-3 items-center">
-          <NSwitch n="lime6 dark:lime5 sm" :model-value="true"> SM </NSwitch>
-          <NSwitch n="red6 dark:red5" :model-value="false"> Normal </NSwitch>
-          <NSwitch :model-value="true" disabled> Disabled </NSwitch>
+          <NSwitch n="lime6 dark:lime5 sm" :model-value="true">
+            SM
+          </NSwitch>
+          <NSwitch n="red6 dark:red5" :model-value="false">
+            Normal
+          </NSwitch>
+          <NSwitch :model-value="true" disabled>
+            Disabled
+          </NSwitch>
           <NSwitch n="purple6 dark:purple5 xl" :model-value="false">
             XL
           </NSwitch>
@@ -110,18 +138,26 @@
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">Tips</div>
+        <div class="n-header-upper">
+          Tips
+        </div>
         <div class="flex flex-col gap-2">
           <NTip n="lime6 dark:lime5" icon="carbon:checkmark-outline">
             Success!
           </NTip>
-          <NTip n="yellow6 dark:yellow5" icon="carbon:warning"> Warning! </NTip>
-          <NTip n="red6 dark:red5" icon="carbon:warning-alt"> Error! </NTip>
+          <NTip n="yellow6 dark:yellow5" icon="carbon:warning">
+            Warning!
+          </NTip>
+          <NTip n="red6 dark:red5" icon="carbon:warning-alt">
+            Error!
+          </NTip>
         </div>
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">Dropdown</div>
+        <div class="n-header-upper">
+          Dropdown
+        </div>
         <div class="flex flex-col gap-2">
           <NDropdown v-model="showDropdown" n="lime6 dark:lime5">
             <template #trigger>
@@ -140,7 +176,9 @@
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">TextInput</div>
+        <div class="n-header-upper">
+          TextInput
+        </div>
         <div class="flex flex-col gap-2">
           <NTextInput
             n="lime6 dark:lime5"
@@ -162,7 +200,9 @@
       </NCard>
 
       <NCard class="p4">
-        <div class="n-header-upper">Dialog</div>
+        <div class="n-header-upper">
+          Dialog
+        </div>
         <div class="flex flex-col gap-2">
           <NButton n="lime6 dark:lime5" @click="showDialog = !showDialog">
             Show Dialog
@@ -171,10 +211,14 @@
             v-model="showDialog"
             class="p4 min-w-100 flex flex-col gap-4"
           >
-            <h1 text-4xl>Hi</h1>
+            <h1 text-4xl>
+              Hi
+            </h1>
             <NTextInput n="lime6 dark:lime5" placeholder="Say something..." />
             <div>
-              <NButton @click="showDialog = false"> Close </NButton>
+              <NButton @click="showDialog = false">
+                Close
+              </NButton>
             </div>
           </NDialog>
         </div>
@@ -184,8 +228,8 @@
 </template>
 
 <script setup lang="ts">
-const isDev = process.dev;
-const showDialog = ref(false);
-const showDropdown = ref(false);
-const radio = ref("a");
+const isDev = process.dev
+const showDialog = ref(false)
+const showDropdown = ref(false)
+const radio = ref('a')
 </script>
