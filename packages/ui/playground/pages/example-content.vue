4d09592f1d8d9a99333325b4c39149914f39288e
Clément Ollivier <clement.o2p@gmail.com>
2022-04-27 15:21:31 +0200

    feat(ui): extend example layout

---
+++ packages/ui/playground/pages/example-content.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtExampleLayout example="test" repo="nuxt/content-next">
+    <template #icon>
+      <IconContent />
+    </template>
+    <div>Hi</div>
+  </NuxtExampleLayout>
+</template>
