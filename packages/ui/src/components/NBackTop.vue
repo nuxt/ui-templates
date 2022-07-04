128fcc88e7a6ff7cc03d066145bf2a7534496012
HomWang <ajake123123@gmail.com>
2022-07-04 10:42:23 +0800

    Modify format

--- packages/ui/src/components/NBackTop.vue
+++ packages/ui/src/components/NBackTop.vue
@@ -1,32 +1,33 @@
 <template>
-    <backTop/>
+  <backTop />
 </template>
 <script setup lang="tsx">
-import { useWindowScroll } from '@vueuse/core'
+import { useWindowScroll } from "@vueuse/core";
 interface IProps {
-  offsetTop?: number,
+  offsetTop?: number;
 }
-const props = defineProps<IProps>()
+const props = defineProps<IProps>();
 
 const { offsetTop } = toRefs(props);
-const offsetTopVal = offsetTop.value ?? 400
+const offsetTopVal = offsetTop.value ?? 400;
 
-const slots = useSlots()
+const slots = useSlots();
 
-const { y } = useWindowScroll()
+const { y } = useWindowScroll();
 
-function callBackTop(){
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
+function callBackTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
-const backTop = ()=>{
-    let newClass = `n-backTop-base ${y.value > offsetTopVal ? '' : 'hidden'}`
-    return <div class={newClass} onClick={callBackTop}>
-        {slots.default()}
+const backTop = () => {
+  let newClass = `n-backTop-base ${y.value > offsetTopVal ? "" : "hidden"}`;
+  return (
+    <div class={newClass} onClick={callBackTop}>
+      {slots.default()}
     </div>
-}
-
+  );
+};
 </script>
