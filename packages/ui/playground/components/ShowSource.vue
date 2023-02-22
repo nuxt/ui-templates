031422634acd9ecd89a89a4b66c5e27345fbede9
Sébastien Chopin <seb@nuxt.com>
2023-02-22 18:45:11 +0100

    chore(ui): update deps and improve docs

---
+++ packages/ui/playground/components/ShowSource.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  src: String
+})
+const show = ref(false)
+const url = computed(() => `https://emgithub.com/embed-v2.js?target=${encodeURIComponent(props.src)}&style=default&type=code&showFullPath=on&showFileMeta=on&showCopy=on`)
+</script>
+
+<template>
+  <span n="xs" class="cursor-pointer n-link n-link-base hover:n-link-hover n-transition" @click="show = !show">
+    {{ show ? 'Hide' : 'Show' }} source
+  </span>
+  <div v-if="show">
+    <Script :src="url" />
+  </div>
+</template>
